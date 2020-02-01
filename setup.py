3dff06b81ca0b7c5c446160081476da1f4871b2d
Chaos249 <ky@ky-aero.localdomain>
2020-02-01 03:54:33 -0500

    first

---
+++ setup.py
@@ -0,0 +1,16 @@
+import pyglet
+
+window = pyglet.window.Window(800, 400, "Test")
+
+def draw_label(name, x_int, y_int):
+	lable = pyglet.text.Label(name, x = x_int, y = y_int)
+	return label
+
+@window.event
+def on_draw():
+	window.clear()
+	draw_label("test label", 100, 100).draw()
+
+if __name__ == "__main__":
+	pyglet.app.run()
+
