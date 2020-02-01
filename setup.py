473e7c4c9f2e93b5192fe5ee2eeacf27fdeb3550
Chaos249 <yagloski@gmail.com>
2020-02-01 04:14:27 -0500

    added some shit 2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import pyglet
 
-window = pyglet.window.Window(800, 400, "Test")
+window = pyglet.window.Window(800, 400, "Modus Operandi")
 
 def draw_label(name, x_int, y_int):
 	label = pyglet.text.Label(name, x = x_int, y = y_int)
@@ -16,7 +16,7 @@ def on_draw():
 	window.clear()
 	image = draw_image("test.png")
 	image.blit(182, 0) # these are cartesian coordinates
-	draw_label("test label", 100, 100).draw()
+	draw_label("test label", 50, 100).draw()
 
 if __name__ == "__main__":
 	pyglet.app.run()
