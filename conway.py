45d39d2e8a5397a2803cff14752449d0ddcd8063
Chaos249 <yagloski@gmail.com>
2020-02-02 00:55:43 -0500

    fixed shit and streamlined, also switched to pygame rather than pyglet cause pyglet sucks ass

---
+++ conway.py
@@ -0,0 +1,95 @@
+
+from prettytable import PrettyTable
+
+import random
+
+# Pure garbage funcitons
+def makeMatrix(x_size, y_size):
+    ptui_matrix = []
+    for x in range(x_size):
+        x_idx = []
+        for y in range(y_size):
+            x_idx.append(0)
+        ptui_matrix.append(x_idx)
+    return ptui_matrix
+
+def prettyPrint(matrix):
+    p = PrettyTable()
+    for row in matrix:
+        p.add_row(row)
+    return p.get_string(header = False, border = False)
+
+# Big boy shit
+def gameOfLife(matrix, size):
+    current_m = matrix
+    future_m = makeMatrix(size, size)
+
+    for x in range(0, size):
+        for y in range(0, size):
+            elem = current_m[x][y]
+            neighbors = countNeighbors(current_m, x, y)
+
+            if elem == 0 and neighbors == 3:
+                future_m[x][y] = 1
+            elif elem == 1 and (neighbors < 2 or neighbors > 3):
+                future_m[x][y] = 0
+            else:
+                future_m[x][y] = elem
+
+    current_m = future_m
+    return current_m
+
+def initGameOfLife(size, elems):
+    matrix = makeMatrix(size, size)
+    matrix = populateMatrix(matrix, size, elems)
+    return matrix
+
+def populateMatrix(matrix, size, elems):
+    for i in range(elems):
+        matrix[random.randrange(0, size)][random.randrange(0, size)] = 1
+    return matrix
+
+def countNeighbors(matrix, x, y):
+    count = 0
+    try:
+        if matrix[x + 1][y] != 0:
+            count += 1
+    except:
+        pass
+    try:
+        if matrix[x + 1][y - 1] != 0:
+            count += 1
+    except:
+        pass
+    try:
+        if matrix[x][y - 1] != 0:
+            count += 1
+    except:
+        pass
+    try:
+        if matrix[x - 1][y - 1] != 0:
+            count += 1
+    except:
+        pass
+    try:
+        if matrix[x - 1][y] != 0:
+            count += 1
+    except:
+        pass
+    try:
+        if matrix[x - 1][y + 1] != 0:
+            count += 1
+    except:
+        pass
+    try:
+        if matrix[x][y + 1] != 0:
+            count += 1
+    except:
+        pass
+    try:
+        if matrix[x + 1][y + 1] != 0:
+            count += 1
+    except:
+        pass
+    return count
+
---
+++ test.py
@@ -0,0 +1,151 @@
+"""import pyglet
+
+window = pyglet.window.Window(800, 400, "Test")
+
+def draw_label(name, x_int, y_int):
+	lable = pyglet.text.Label(name, x = x_int, y = y_int)
+	return label
+
+@window.event
+def on_draw():
+	window.clear()
+	draw_label("test label", 100, 100).draw()
+
+if __name__ == "__main__":
+	pyglet.app.run()
+
+def main():
+    info = pg.display.Info()
+    screen = pg.display.set_mode((info.current_w, info.current_h), pg.FULLSCREEN)
+    screen_rect = screen.get_rect()
+    font = pg.font.Font(None, 45)
+    clock = pg.time.Clock()
+    color = (randrange(256), randrange(256), randrange(256))
+    txt = font.render(random_letters(randrange(5, 21)), True, color)
+    timer = 10
+    done = False
+
+    while not done:
+        for event in pg.event.get():
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    done = True
+
+        timer -= 1
+        # Update the text surface and color every 10 frames.
+        if timer <= 0:
+            timer = 10
+            color = (randrange(256), randrange(256), randrange(256))
+            txt = font.render(random_letters(randrange(5, 21)), True, color)
+
+        screen.fill((30, 30, 30))
+        screen.blit(txt, txt.get_rect(center=screen_rect.center))
+
+        pg.display.flip()
+        clock.tick(30)
+
+
+if __name__ == '__main__':
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
+shareimprove this answer
+edited"""
+
+"""
+ Example program to show using an array to back a grid on-screen.
+ 
+ Sample Python/Pygame Programs
+ Simpson College Computer Science
+ http://programarcadegames.com/
+ http://simpson.edu/computer-science/
+ 
+ Explanation video: http://youtu.be/mdTeqiWyFnc
+"""
+
+import pygame
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 20
+HEIGHT = 20
+
+# This sets the margin between each cell
+MARGIN = 5
+
+# Create a 2 dimensional array. A two dimensional
+# array is simply a list of lists.
+grid = []
+for row in range(10):
+    # Add an empty array that will hold each cell
+    # in this row
+    grid.append([])
+    for column in range(10):
+        grid[row].append(0)  # Append a cell
+
+# Set row 1, cell 5 to one. (Remember rows and
+# column numbers start at zero.)
+grid[1][5] = 1
+
+# Initialize pygame
+pygame.init()
+
+# Set the HEIGHT and WIDTH of the screen
+WINDOW_SIZE = [255, 255]
+screen = pygame.display.set_mode(WINDOW_SIZE)
+
+# Set title of screen
+pygame.display.set_caption("Array Backed Grid")
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# -------- Main Program Loop -----------
+while not done:
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # User clicks the mouse. Get the position
+            pos = pygame.mouse.get_pos()
+            # Change the x/y screen coordinates to grid coordinates
+            column = pos[0] // (WIDTH + MARGIN)
+            row = pos[1] // (HEIGHT + MARGIN)
+            # Set that location to one
+            grid[row][column] = 1
+            print("Click ", pos, "Grid coordinates: ", row, column)
+
+    # Set the screen background
+    screen.fill(BLACK)
+
+    # Draw the grid
+    for row in range(10):
+        for column in range(10):
+            color = WHITE
+            if grid[row][column] == 1:
+                color = GREEN
+            pygame.draw.rect(screen,
+                             color,
+                             [(MARGIN + WIDTH) * column + MARGIN,
+                              (MARGIN + HEIGHT) * row + MARGIN,
+                              WIDTH,
+                              HEIGHT])
+
+    # Limit to 60 frames per second
+    clock.tick(60)
+
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+# Be IDLE friendly. If you forget this line, the program will 'hang'
+# on exit.
+pygame.quit()
