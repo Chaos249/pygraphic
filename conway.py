59de44888a633d22056170f6df9631f526e8303c
Chaos249 <yagloski@gmail.com>
2020-02-04 09:12:54 -0500

    made movement actually work

--- conway.py
+++ conway.py
@@ -23,12 +23,15 @@ def prettyPrint(matrix):
 def gameOfLife(matrix, size):
     current_m = matrix
     future_m = makeMatrix(size, size)
+    state = True
 
     for x in range(0, size):
         for y in range(0, size):
             elem = current_m[x][y]
             neighbors = countNeighbors(current_m, x, y)
 
+            if elem == 2:
+                return False
             if elem == 0 and neighbors == 3:
                 future_m[x][y] = 1
             elif elem == 1 and (neighbors < 2 or neighbors > 3):
@@ -37,7 +40,7 @@ def gameOfLife(matrix, size):
                 future_m[x][y] = elem
 
     current_m = future_m
-    return current_m
+    return current_m, state
 
 def initGameOfLife(size, elems):
     matrix = makeMatrix(size, size)
@@ -52,44 +55,64 @@ def populateMatrix(matrix, size, elems):
 def countNeighbors(matrix, x, y):
     count = 0
     try:
-        if matrix[x + 1][y] != 0:
+        if matrix[x + 1][y] == 1:
             count += 1
     except:
         pass
     try:
-        if matrix[x + 1][y - 1] != 0:
+        if matrix[x + 1][y - 1] == 1:
             count += 1
     except:
         pass
     try:
-        if matrix[x][y - 1] != 0:
+        if matrix[x][y - 1] == 1:
             count += 1
     except:
         pass
     try:
-        if matrix[x - 1][y - 1] != 0:
+        if matrix[x - 1][y - 1] == 1:
             count += 1
     except:
         pass
     try:
-        if matrix[x - 1][y] != 0:
+        if matrix[x - 1][y] == 1:
             count += 1
     except:
         pass
     try:
-        if matrix[x - 1][y + 1] != 0:
+        if matrix[x - 1][y + 1] == 1:
             count += 1
     except:
         pass
     try:
-        if matrix[x][y + 1] != 0:
+        if matrix[x][y + 1] == 1:
             count += 1
     except:
         pass
     try:
-        if matrix[x + 1][y + 1] != 0:
+        if matrix[x + 1][y + 1] == 1:
             count += 1
     except:
         pass
     return count
+
+def addMore(matrix, size):
+    for i in range(size):
+        matrix[0][i] = 1
+        #matrix[1][i] = 1
+    for i in range(size):
+        matrix[-1][i] = 1
+        #matrix[-2][i] = 1
+    for i in range(size):
+        matrix[i][0] = 1
+        #matrix[i][1] = 1
+        matrix[i][-1] = 1
+        #matrix[i][-2] = 1
+    return matrix
+
+def setCharacter(matrix, size):
+    matrix[size//2][size//2] = 2
+    player_index = [size//2, size//2]
+    return player_index
+
 
--- setup.py
+++ setup.py
@@ -1,20 +1,32 @@
-import conway
+import conway as cw
 
 import pygame
 pygame.init()
 
 BLACK = (10, 10, 10)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 MARGIN = 0
 W = 5
 H = 5
 
 clock = pygame.time.Clock()
 
-def renderGrid(size, elems):
-    matrix = conway.initGameOfLife(size, elems)
+def renderGrid(size, elems, additionals):
+    count = 0
+    matrix = cw.initGameOfLife(size, elems)
+    player_index = cw.setCharacter(matrix, size)
+
     done = False
     while not done:
+        count += 1
+
+        player_index = player_index
+        matrix, player_index = controlPlayer(matrix, player_index)
+
+        if additionals:
+            if count % 100 == 0:
+                matrix = cw.addMore(matrix, size)
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -26,24 +38,46 @@ def renderGrid(size, elems):
                 color = BLACK
                 if matrix[row][column] == 1:
                     color = WHITE
+                if matrix[row][column] == 2:
+                    color = RED
                 pygame.draw.rect(screen, color, [(MARGIN + W) * column + MARGIN, (MARGIN + H) * row + MARGIN, W, H])
 
-        matrix = conway.gameOfLife(matrix, size)
+        matrix = cw.gameOfLife(matrix, size)
 
         clock.tick(60)
+
         pygame.display.flip()
 
-def initDisplay(size, fullscreen):
+def initDisplay(size):
     pygame.display.set_caption("Modus Operandi")
-    if fullscreen:
-        screen = pygame.display.set_mode([size[0]*5, size[1]*5] , pygame.FULLSCREEN)
-    else:
-        screen = pygame.display.set_mode([size[0]*5, size[1]*5])
+    screen = pygame.display.set_mode([size[0]*5, size[1]*5])
     return screen
 
-if __name__ == "__main__":
-    size = 80
-    elems = 1000
+def controlPlayer(matrix, player_index):
+    key = pygame.key.get_pressed()
 
-    screen = initDisplay([size, size], False)
-    renderGrid(size, elems)
+    if key[pygame.K_LEFT]:
+        matrix[player_index[1]][player_index[0]] = 0
+        player_index[0] = player_index[0] - 2
+        matrix[player_index[1]][player_index[0]] = 2
+    if key[pygame.K_RIGHT]:
+        matrix[player_index[1]][player_index[0]] = 0
+        player_index[0] = player_index[0] + 2
+        matrix[player_index[1]][player_index[0]] = 2
+    if key[pygame.K_UP]:
+        matrix[player_index[1]][player_index[0]] = 0
+        player_index[1] = player_index[1] - 2
+        matrix[player_index[1]][player_index[0]] = 2
+    if key[pygame.K_DOWN]:
+        matrix[player_index[1]][player_index[0]] = 0
+        player_index[1] = player_index[1] + 2
+        matrix[player_index[1]][player_index[0]] = 2
+
+    return matrix, player_index
+
+if __name__ == "__main__":
+    size = 150
+    elems = 2025
+
+    screen = initDisplay([size, size])
+    renderGrid(size, elems, True)
